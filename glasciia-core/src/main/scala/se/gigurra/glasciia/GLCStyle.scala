1318c273eef1ded6ed9943b9f6925c773dc9d996
Johan Kjölhede <gigurra@gmail.com>
2016-09-27 23:14:48 +0200

    fix naming

--- glasciia-core/src/main/scala/se/gigurra/glasciia/GLCStyle.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/GLCStyle.scala
@@ -6,11 +6,11 @@ import se.gigurra.glasciia.impl.GlConstants
 
 import scala.language.implicitConversions
 
-trait CStyleGL extends GlConstants {
+trait GLCStyle extends GlConstants {
   def gl: GL20 = Gdx.gl20
-  implicit def gl2gl(self: CStyleGL): GL20 = gl
+  implicit def gl2gl(self: GLCStyle): GL20 = gl
 }
 /**
   * Created by johan on 2016-09-27.
   */
-object CStyleGL extends CStyleGL
+object GLCStyle extends GLCStyle
--- glasciia-core/src/main/scala/se/gigurra/glasciia/impl/GlWindowFunctions.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/impl/GlWindowFunctions.scala
@@ -1,12 +1,12 @@
 package se.gigurra.glasciia.impl
 
 import com.badlogic.gdx.{Gdx, Graphics}
-import se.gigurra.glasciia.{CStyleGL, Window}
+import se.gigurra.glasciia.{GLCStyle, Window}
 
 /**
   * Created by johan on 2016-09-27.
   */
-trait GlWindowFunctions extends CStyleGL {  _: Window =>
+trait GlWindowFunctions extends GLCStyle {  _: Window =>
 
   def drawFrame(content: => Unit): Unit = {
     gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
