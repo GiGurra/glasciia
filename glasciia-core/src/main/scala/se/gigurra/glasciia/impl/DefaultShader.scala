6ae97b8e12feafc725443c7fe31db7928ee3aa00
Johan Kjölhede <gigurra@gmail.com>
2016-10-01 23:28:16 +0200

    cleanup

--- glasciia-core/src/main/scala/se/gigurra/glasciia/impl/DefaultShader.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/impl/DefaultShader.scala
@@ -9,7 +9,7 @@ import se.gigurra.io.LoadString
   * Created by johan on 2016-10-01.
   */
 trait DefaultShader {
-  val defaultShader: ShaderProgram = new ShaderProgram(
+  val defaultShader = new ShaderProgram(
     LoadString.from("shaders/default-shader.vert").getOrElse(throw new FileNotFoundException(s"Could not find shaders/default-shader.vert")),
     LoadString.from("shaders/default-shader.frag").getOrElse(throw new FileNotFoundException(s"Could not find shaders/default-shader.frag"))
   )
