c1f82baa837e23053a0aa9c2df436a48352dfffe
Johan Kjölhede <gigurra@gmail.com>
2016-10-01 22:48:56 +0200

    cleanup

--- glasciia-core/src/main/scala/se/gigurra/glasciia/impl/Cameras.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/impl/Cameras.scala
@@ -1,6 +1,6 @@
 package se.gigurra.glasciia.impl
 
-import com.badlogic.gdx.graphics.{Camera => GdxCamera, OrthographicCamera, PerspectiveCamera}
+import com.badlogic.gdx.graphics.{Camera, OrthographicCamera, PerspectiveCamera}
 
 /**
   * Created by johan on 2016-10-01.
@@ -9,7 +9,7 @@ trait Cameras {
 
   val orthographicCamera = new OrthographicCamera
   val perspectiveCamera = new PerspectiveCamera
-  var camera: GdxCamera = orthographicCamera
+  var camera: Camera = orthographicCamera
 
   def setOrtho(yDown: Boolean, width: Float, height: Float): Unit = {
     orthographicCamera.setToOrtho(yDown, width, height)
