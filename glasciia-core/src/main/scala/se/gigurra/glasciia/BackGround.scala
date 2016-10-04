6faa18cd1378da4132d03f38830617a5c75276a4
Johan Kjölhede <gigurra@gmail.com>
2016-10-04 23:56:22 +0200

    Start working on backgrounds

--- glasciia-core/src/main/scala/se/gigurra/glasciia/BackGround.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/BackGround.scala
@@ -1,5 +1,8 @@
 package se.gigurra.glasciia
 
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import se.gigurra.glasciia.BackGround.Layer.Piece
 import se.gigurra.math.Box
 
 /**
@@ -11,11 +14,17 @@ case class BackGround(layers: Seq[BackGround.Layer]) {
 }
 
 object BackGround {
-  case class Layer(bounds: Box[Float],
-                   translationScale: Float) {
-
+  case class Layer(bounds: Box[Float], translationScale: Float, pieces: Seq[Piece]) {
+    def piecesInView(camera: Camera): Seq[Piece] = {
+      pieces.filter(_.isInView(camera))
+    }
   }
 
-
-
+  object Layer {
+    case class Piece(bounds: Box[Float], region: TextureRegion) {
+      def isInView(camera: Camera): Boolean = {
+        ???
+      }
+    }
+  }
 }
