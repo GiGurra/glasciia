93b28fd94bf7a890765fc962005af3b50378e870
Johan Kjölhede <gigurra@gmail.com>
2016-10-05 20:53:32 +0200

    Add BackGroundBuilder

--- glasciia-core/src/main/scala/se/gigurra/glasciia/BackGround.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/BackGround.scala
@@ -1,30 +1,38 @@
 package se.gigurra.glasciia
 
-import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.g2d.TextureRegion
-import se.gigurra.glasciia.BackGround.Layer.Piece
 import se.gigurra.math.Box
+import scala.collection.mutable
 
 /**
   * @param layers
   *               In draw order, i.e. [Furthest away ... Closest]
   */
-case class BackGround(layers: Seq[BackGround.Layer]) {
-
-}
+case class BackGround(layers: Seq[BackGroundLayer])
+case class BackGroundLayer(translationScale: Float, pieces: Seq[BackGroundPiece])
+case class BackGroundPiece(bounds: Box[Float], region: TextureRegion)
 
 object BackGround {
-  case class Layer(bounds: Box[Float], translationScale: Float, pieces: Seq[Piece]) {
-    def piecesInView(camera: Camera): Seq[Piece] = {
-      pieces.filter(_.isInView(camera))
-    }
-  }
 
-  object Layer {
-    case class Piece(bounds: Box[Float], region: TextureRegion) {
-      def isInView(camera: Camera): Boolean = {
-        ???
+  def builder: _impl.Builder = new _impl.Builder()
+
+  object _impl {
+
+    class Builder(private[BackGround] var layers: mutable.Buffer[BackGroundLayer] = mutable.Buffer.empty) {
+      def newLayer(translationScale: Float = 1.0f): LayerBuilder = new LayerBuilder(this, translationScale)
+      def build(): BackGround = new BackGround(layers)
+    }
+
+    class LayerBuilder(builder: Builder, translationScale: Float, pieces: mutable.Buffer[BackGroundPiece] = mutable.Buffer.empty) {
+      def addPiece(bounds: Box[Float], region: TextureRegion): LayerBuilder = {
+        pieces += BackGroundPiece(bounds, region)
+        this
+      }
+      def build(): Builder = {
+        builder.layers += BackGroundLayer(translationScale, pieces)
+        builder
       }
     }
   }
+
 }
