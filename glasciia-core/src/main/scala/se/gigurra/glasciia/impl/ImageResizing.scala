34162b89e67fbab06d9150d03c32a026a7158860
Johan Kjölhede <gigurra@gmail.com>
2016-10-05 18:10:08 +0200

    remove debug printout

--- glasciia-core/src/main/scala/se/gigurra/glasciia/impl/ImageResizing.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/impl/ImageResizing.scala
@@ -8,7 +8,6 @@ import se.gigurra.math.Vec2
   */
 trait ImageResizing {
   def resize(source: Pixmap, size: Vec2[Int], deleteSource: Boolean, newFormat: Option[Pixmap.Format] = None): Pixmap = {
-    println(source.getFormat)
     val pm2 = new Pixmap(size.x, size.y, newFormat.getOrElse(source.getFormat))
     // src: Pixmap,        srcx, srcy,        srcWidth,       srcHeight,  dstx, dsty, dstWidth, dstHeight
     pm2.drawPixmap(source,    0,    0, source.getWidth, source.getHeight,    0,    0,   size.x,    size.y)
