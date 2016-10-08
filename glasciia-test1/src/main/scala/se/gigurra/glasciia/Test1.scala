b2290202e5298f02dfbade846c06fe5b03516472
Johan Kjölhede <gigurra@gmail.com>
2016-10-08 11:25:35 +0200

    cleanup

--- glasciia-test1/src/main/scala/se/gigurra/glasciia/Test1.scala
+++ glasciia-test1/src/main/scala/se/gigurra/glasciia/Test1.scala
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode
 import com.badlogic.gdx.graphics.{Color, Cursor}
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.FillViewport
 import se.gigurra.glasciia.App.{GlConf, WindowConf}
 import se.gigurra.glasciia.AppEvent._
 import se.gigurra.glasciia.Glasciia._
@@ -132,122 +133,122 @@ object Test1 {
 
         val background = app.resource[MultiLayer[Image]]("background-0")
 
-              canvas.setOrtho(
-                yDown = false,
-                width = canvas.width,
-                height = canvas.height
-              )
+        canvas.setOrtho(
+          yDown = false,
+          width = canvas.width,
+          height = canvas.height
+        )
 
-              canvas.drawFrame(
-                background = Color.DARK_GRAY,
-                camPos = cameraPos) {
+        canvas.drawFrame(
+          background = Color.DARK_GRAY,
+          camPos = cameraPos) {
 
-                canvas.drawBackGround(background)
+          canvas.drawBackGround(background)
 
-                canvas.drawText(
-                  text = "A",
-                  font = monospaceFont,
-                  color = Color.GREEN,
-                  at = Vec2(140, 140),
-                  rotate = 45,
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "A",
+            font = monospaceFont,
+            color = Color.GREEN,
+            at = Vec2(140, 140),
+            rotate = 45,
+            scale = 50
+          )
 
-                canvas.drawText(
-                  text = "B",
-                  font = monospaceFont,
-                  color = Color.GREEN,
-                  at = Vec2(240, 240),
-                  rotate = -45,
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "B",
+            font = monospaceFont,
+            color = Color.GREEN,
+            at = Vec2(240, 240),
+            rotate = -45,
+            scale = 50
+          )
 
-                canvas.drawText(
-                  text = "CDEFG",
-                  font = monospaceFont,
-                  color = Color.GREEN,
-                  at = Vec2(400, 400),
-                  rotate = 180 + canvas.drawTime.toFloat * 360.0f,
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "CDEFG",
+            font = monospaceFont,
+            color = Color.GREEN,
+            at = Vec2(400, 400),
+            rotate = 180 + canvas.drawTime.toFloat * 360.0f,
+            scale = 50
+          )
 
-                canvas.drawText(
-                  text = "UL",
-                  font = monospaceFont,
-                  color = Color.RED,
-                  at = Vec2(0, 480),
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "UL",
+            font = monospaceFont,
+            color = Color.RED,
+            at = Vec2(0, 480),
+            scale = 50
+          )
 
-                canvas.drawText(
-                  text = "LL",
-                  font = monospaceFont,
-                  color = Color.GREEN,
-                  at = Vec2(0, 50),
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "LL",
+            font = monospaceFont,
+            color = Color.GREEN,
+            at = Vec2(0, 50),
+            scale = 50
+          )
 
-                canvas.drawText(
-                  text = "UR",
-                  font = monospaceFont,
-                  color = Color.BLUE,
-                  at = Vec2(640 - 50 * 2 * monospaceFont.spaceWidth(), 480),
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "UR",
+            font = monospaceFont,
+            color = Color.BLUE,
+            at = Vec2(640 - 50 * 2 * monospaceFont.spaceWidth(), 480),
+            scale = 50
+          )
 
-                canvas.drawText(
-                  text = "LR",
-                  font = monospaceFont,
-                  color = Color.BLACK,
-                  at = Vec2(640 - 50 * 2 * monospaceFont.spaceWidth(), 50),
-                  scale = 50
-                )
+          canvas.drawText(
+            text = "LR",
+            font = monospaceFont,
+            color = Color.BLACK,
+            at = Vec2(640 - 50 * 2 * monospaceFont.spaceWidth(), 50),
+            scale = 50
+          )
 
-                canvas.drawAnimation(
-                  animation = walkingDudeAnimation,
-                  at = Vec2(400, 100),
-                  scale = Vec2(120.0f, 200.0f)
-                )
+          canvas.drawAnimation(
+            animation = walkingDudeAnimation,
+            at = Vec2(400, 100),
+            scale = Vec2(120.0f, 200.0f)
+          )
 
-                canvas.drawImage(
-                  image = testImage,
-                  at = Vec2(100, 300),
-                  scale = Vec2(120.0f, 120.0f)
-                )
+          canvas.drawImage(
+            image = testImage,
+            at = Vec2(100, 300),
+            scale = Vec2(120.0f, 120.0f)
+          )
 
-                canvas.drawParticles(
-                  effect = effect1,
-                  at = testEffectPosition(canvas.drawTime)
-                )
+          canvas.drawParticles(
+            effect = effect1,
+            at = testEffectPosition(canvas.drawTime)
+          )
 
-                canvas.drawParticles(
-                  effect = effect2,
-                  at = testEffectPosition(canvas.drawTime)
-                )
+          canvas.drawParticles(
+            effect = effect2,
+            at = testEffectPosition(canvas.drawTime)
+          )
 
-                canvas.drawParticles(
-                  effect = effect3,
-                  at = mouseWorldPos,
-                  angle = canvas.drawTime.toFloat * 180.0f
-                )
+          canvas.drawParticles(
+            effect = effect3,
+            at = mouseWorldPos,
+            angle = canvas.drawTime.toFloat * 180.0f
+          )
 
-                canvas.drawText(
-                  text = "CAM",
-                  font = monospaceFont,
-                  color = Color.WHITE,
-                  anchor = Anchor.CC,
-                  scale = 25,
-                  at = cameraPos
-                )
+          canvas.drawText(
+            text = "CAM",
+            font = monospaceFont,
+            color = Color.WHITE,
+            anchor = Anchor.CC,
+            scale = 25,
+            at = cameraPos
+          )
 
-                canvas.drawText(
-                  text = "CAM",
-                  font = monospaceFontMasked,
-                  color = Color.WHITE,
-                  anchor = Anchor.CC,
-                  scale = 25,
-                  at = cameraPos - Vec2(0.0f, 50.0f)
-                )
+          canvas.drawText(
+            text = "CAM",
+            font = monospaceFontMasked,
+            color = Color.WHITE,
+            anchor = Anchor.CC,
+            scale = 25,
+            at = cameraPos - Vec2(0.0f, 50.0f)
+          )
 
         }
       case input: InputEvent =>
