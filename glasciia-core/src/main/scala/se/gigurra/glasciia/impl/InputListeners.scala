af3c3fc85f6690ff1aa4e67f0286df6ed806cac1
Johan Kjölhede <gigurra@gmail.com>
2016-10-09 14:30:24 +0200

    Controls... what to do with them :S

--- glasciia-core/src/main/scala/se/gigurra/glasciia/impl/InputListeners.scala
+++ glasciia-core/src/main/scala/se/gigurra/glasciia/impl/InputListeners.scala
@@ -2,7 +2,8 @@ package se.gigurra.glasciia.impl
 import com.badlogic.gdx.scenes.scene2d.utils.{ClickListener, FocusListener}
 import com.badlogic.gdx.scenes.scene2d.utils.FocusListener.FocusEvent
 import com.badlogic.gdx.scenes.scene2d._
-import se.gigurra.glasciia.AppEvent.{CharTyped, KeyDown, KeyUp}
+import se.gigurra.glasciia.AppEvent
+import se.gigurra.glasciia.AppEvent.{CharTyped, KeyDown, KeyUp, KeyboardEvent}
 
 import scala.language.{implicitConversions, reflectiveCalls}
 
@@ -13,20 +14,51 @@ import scala.language.{implicitConversions, reflectiveCalls}
 trait InputListeners {
 
   type CanAddListener = { def addListener(l: EventListener): Boolean }
+  type CanFireEvents = { def fire (event: Event): Boolean }
+  type CanFireAndReceiveEvents = CanAddListener with CanFireEvents
   type Emitter = Actor
 
+  implicit class canTakeKeyboardFocus[Subject <: Actor](self: Subject) {
+
+    def setKeyFocus(): Subject = {
+      self.getStage.setKeyboardFocus(self)
+      self
+    }
+  }
+
+  implicit class canFireEvents[Shooter <: CanFireEvents](self: Shooter) {
+
+    def click(): Shooter = {
+      val event1 = new InputEvent()
+      event1.setType(InputEvent.Type.touchDown)
+      self.fire(event1)
+
+      val event2 = new InputEvent()
+      event2.setType(InputEvent.Type.touchUp)
+      self.fire(event2)
+
+      self
+    }
+  }
+
+  implicit class canFireAndReceiveEvents[Subject <: CanFireAndReceiveEvents](subject: Subject) {
+
+    def mapKeyDownToClick(vKey: Int, consume: Boolean = true): Subject = {
+      subject.on({ case KeyDown(`vKey`) => subject.click()}, consume = consume)
+      subject
+    }
+  }
+
   implicit class canConsumeEvents[Receiver <: CanAddListener](self: Receiver) {
-    def onKeyDown(f: PartialFunction[KeyDown, Unit], consume: Boolean = true): InputListener = addAndReturnListener(keyDown(f, consume))
-    def onKeyUp(f: PartialFunction[KeyUp, Unit], consume: Boolean = true): InputListener = addAndReturnListener(keyUp(f, consume))
-    def onCharTyped(f: PartialFunction[CharTyped, Unit], consume: Boolean = true): InputListener = addAndReturnListener(charTyped(f, consume))
+    def on[R](f: PartialFunction[AppEvent.InputEvent, R], consume: Boolean = true): InputListener = addAndReturnListener(keyListener(f, consume))
 
-    def onClicked(f: (Receiver, Float, Float) => Unit): InputListener = addAndReturnListener(click((x,y) => f(self, x,y)))
-    def onClicked(f: (Float, Float) => Unit): InputListener = onClicked((_, x, y) => f(x,y))
-    def onClicked(f: => Unit): InputListener = onClicked((_,_) => f)
+    def onClick[R](f: (Receiver, Float, Float) => R): InputListener = addAndReturnListener(clickListener((x, y) => f(self, x,y)))
+    def onClick[R](f: (Float, Float) => R): InputListener = onClick((_: Receiver, x: Float, y: Float) => f(x,y))
+    def onClick[R](f: => R): InputListener = onClick((_: Float, _: Float) => f)
 
-    def onKeyFocusChange(f: (Receiver, Emitter, Boolean) => Unit): FocusListener = addAndReturnListener(focus((emitter, newState) => f(self, emitter, newState)))
-    def onKeyFocusChange(f: (Receiver, Boolean) => Unit): FocusListener = addAndReturnListener(focus((emitter, newState) => f(self, newState)))
-    def onKeyFocusChange(f: Boolean => Unit): FocusListener = addAndReturnListener(focus((emitter, newState) => f(newState)))
+    def onKeyFocusChange(f: (Receiver, Emitter, Boolean) => Unit): FocusListener = addAndReturnListener(focusListener((emitter, newState) => f(self, emitter, newState)))
+    def onKeyFocusChange(f: (Receiver, Boolean) => Unit): FocusListener = addAndReturnListener(focusListener((emitter, newState) => f(self, newState)))
+    def onKeyFocusChange(f: Boolean => Unit): FocusListener = addAndReturnListener(focusListener((emitter, newState) => f(newState)))
     def onKeyFocusGained(f: (Receiver, Emitter) => Unit): FocusListener = onKeyFocusChange((self, emitter, newState) => if (newState) f(self, emitter))
     def onKeyFocusGained(f: Receiver => Unit): FocusListener = onKeyFocusGained({ (a, _) => f(a) }: (Receiver, Emitter) => Unit)
     def onKeyFocusGained(f: => Unit): FocusListener = onKeyFocusGained(_ => f)
@@ -47,29 +79,23 @@ trait InputListeners {
     }
   }
 
-  def focus(f: (Emitter, Boolean) => Unit): FocusListener = new FocusListener {
+  private def focusListener(f: (Emitter, Boolean) => Unit): FocusListener = new FocusListener {
     override def keyboardFocusChanged(event: FocusEvent, emitter: Emitter, focused: Boolean): Unit = {
       f(emitter, focused)
     }
   }
 
-  def click(f: (Float, Float) => Unit): InputListener = new ClickListener {
+  private def clickListener(f: (Float, Float) => Unit): InputListener = new ClickListener {
     override def clicked(event: InputEvent, x: Float, y: Float): Unit = f(x, y)
   }
 
-  def keyDown(f: PartialFunction[KeyDown, Unit], consume: Boolean): InputListener = new InputListener {
+  private def keyListener[R](f: PartialFunction[KeyboardEvent, R], consume: Boolean): InputListener = new InputListener {
     override def keyDown(event: InputEvent, keycode: Int): Boolean = tryConsume(f, KeyDown(keycode)) && consume
-  }
-
-  def keyUp(f: PartialFunction[KeyUp, Unit], consume: Boolean): InputListener = new InputListener {
     override def keyUp(event: InputEvent, keycode: Int): Boolean = tryConsume(f, KeyUp(keycode)) && consume
-  }
-
-  def charTyped(f: PartialFunction[CharTyped, Unit], consume: Boolean): InputListener = new InputListener {
     override def keyTyped(event: InputEvent, character: Char): Boolean = tryConsume(f, CharTyped(character)) && consume
   }
 
-  private def tryConsume[Event](f: PartialFunction[Event, Unit], event: Event): Boolean = {
+  private def tryConsume[Event, R](f: PartialFunction[Event, R], event: Event): Boolean = {
     f.lift.apply(event) match {
       case Some(_) => true
       case None => false
--- glasciia-test1/src/main/scala/se/gigurra/glasciia/test1/testcomponents/queLoadResources.scala
+++ glasciia-test1/src/main/scala/se/gigurra/glasciia/test1/testcomponents/queLoadResources.scala
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.graphics.{Color, Pixmap}
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode
 import com.badlogic.gdx.graphics.g2d.{BitmapFont, TextureRegion}
-import com.badlogic.gdx.scenes.scene2d.{Event, InputListener}
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
 import com.badlogic.gdx.scenes.scene2d.ui.{Label, TextButton}
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle
@@ -124,35 +123,25 @@ object queLoadResources {
     val startBtn = new TextButton("start", table.skin).fontScale(1.5f * itemScale)
     val optionsBtn = new TextButton("options", table.skin).fontScale(1.5f * itemScale)
     val exitBtn = new TextButton("exit", table.skin).fontScale(1.5f * itemScale)
-
-    startBtn.onKeyDown{ case KeyDown(Keys.ENTER) => menu.hide() }
-    startBtn.onClicked(menu.hide())
-    exitBtn.onKeyDown{ case KeyDown(Keys.ENTER) => app.close() }
-    exitBtn.onClicked(app.close())
-
-    optionsBtn.onKeyDown{ case KeyDown(Keys.ENTER) => app.addResource("controls-inverted", !app.getResource[Boolean]("controls-inverted").getOrElse(false)) }
-
-    app.addResource("gui:main-menu:table", table)
-    app.addResource("gui:main-menu:btn1", startBtn)
-    app.addResource("gui:main-menu:btn2", optionsBtn)
-    app.addResource("gui:main-menu:btn3", exitBtn)
-
     val menuButtons = Seq(startBtn, optionsBtn, exitBtn)
+
+    startBtn.onClick(menu.hide())
+    exitBtn.onClick(app.close())
+    optionsBtn.onClick(app.addResource("controls-inverted", !app.getResource[Boolean]("controls-inverted").getOrElse(false)))
+
     for ((btn, i) <- menuButtons.zipWithIndex) {
-      def go(di: Int) = stage.setKeyboardFocus(menuButtons((menuButtons.size + i + di) % menuButtons.size))
-      btn.onKeyDown {
+      def go(di: Int) = menuButtons((menuButtons.size + i + di) % menuButtons.size).setKeyFocus()
+      btn.on {
         case KeyDown(Keys.DOWN) => go(+1)
         case KeyDown(Keys.UP)   => go(-1)
       }
       btn.onKeyFocusGained(_.setStyle(menu.style[TextButtonStyle]("default:keyboard-focus")))
       btn.onKeyFocusLost(_.setStyle(menu.style[TextButtonStyle]))
+      btn.mapKeyDownToClick(Keys.ENTER)
+      btn.onClick(btn.setKeyFocus())
     }
 
-    menu.stage.setKeyboardFocus(startBtn)
-    menu.stage.blockInputEventPropagation()
-
     val btnWidth = 200.0f
-
     table
       .rw(_.cell().height(120))
       .rw(_.cell(new Label("THE COOLEST GAME", table.skin).fontScale(2.5f * itemScale)).center())
@@ -167,7 +156,8 @@ object queLoadResources {
       .rw(_.cell(new Label("Copyright (c) Idiot ltd", table.skin).fontScale(0.5f * itemScale)).center())
       .rw(_.cell().height(menuItemPad * 0.5f))
 
-
+    startBtn.setKeyFocus()
+    stage.blockInputEventPropagation()
   }
 
   /*
