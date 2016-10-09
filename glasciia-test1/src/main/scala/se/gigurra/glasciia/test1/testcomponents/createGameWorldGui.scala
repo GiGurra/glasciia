107a85a8ecc29aa88a3c0bb1fa41c2d9151bb3f1
Johan Kjölhede <gigurra@gmail.com>
2016-10-09 18:44:05 +0200

    I get the gui!

--- glasciia-test1/src/main/scala/se/gigurra/glasciia/test1/testcomponents/createGameWorldGui.scala
+++ glasciia-test1/src/main/scala/se/gigurra/glasciia/test1/testcomponents/createGameWorldGui.scala
@@ -41,34 +41,30 @@ object createGameWorldGui {
     //          //                      //          //
     //////////////////////////////////////////////////
 
-    // 1. status bar. 0-7% of screen height
-    // 2. game draw area (in general, we shouldn't put any UI components here) 7%-75% of screen height
-    // 3. mini map. 75%-100% of screen height
-    // 4. unit bar. 80%-100% of screen height
-    // 5. Command area. 75%-100% of screen height
+    // 1. status bar. 0->10% of screen height
+    // 2. game draw area (in general, we shouldn't put any UI components here) 7%->75% of screen height
+    // 3. mini map. 75%->100% of screen height
+    // 4. unit bar. 80%->100% of screen height
+    // 5. Command area. 75%->100% of screen height
 
-    val statusBar = Gui(new Table(skin), fillParent = false)
-    val gameArea = Gui(new Table(skin), fillParent = false)
-    //val miniMap = Gui(new Table(skin), fillParent = true)
-    val unitBar = Gui(new Table(skin), fillParent = false)
-    val commandArea = Gui(new Table(skin), fillParent = false)
-    val bottom = Gui(new Table(skin), fillParent = false, debug = true)
+    val statusBar = new Table(skin)
+    val gameArea = new Table(skin)
+    val miniMap = new Table(skin)
+    val unitBar = new Table(skin)
+    val commandArea = new Table(skin)
 
-    val button = new TextButton("minimap", skin)
-   // miniMap.setSize(640, 100)
-  //  miniMap.setFillParent(true)
-    val miniMap = skin.newInstance("fill", Color.YELLOW)
-
-    bottom.table.rw { r =>
-      r.cellImg("fill", Color.YELLOW).prefWidth(640.0f * 0.33f).growY()
-      r.cellImg("fill",  Color.GREEN).prefWidth(640.0f * 0.6f).prefHeight(0.20f * 480).growX().bottom()
-      r.cellImg("fill", Color.YELLOW).prefWidth(640.0f * 0.33f).growY()
+    root.rw {
+      _.cell(statusBar).colspan(3).growX().prefHeight(640.0f * 0.10f)
+    }
+    root.rw {
+      _.cell(gameArea).colspan(3).grow()
+    }
+    root.rw { r =>
+      r.cell(miniMap).prefWidth(640.0f * 0.33f).prefHeight(480.0f * 0.33f).fill()
+      r.cell(unitBar).prefHeight(0.20f * 480).expandX().bottom().fillX()
+      r.cell(commandArea).prefWidth(640.0f * 0.33f).prefHeight(480.0f * 0.33f).fill()
       r.bottom()
     }
-
-    root.rw { r => r.cell(statusBar.table).growX().height(640.0f * 0.07f) }
-    root.rw { r => r.cell(gameArea.table).grow() }
-    root.rw { r => r.cell(bottom.table).growX().height(480.0f * 0.33f).bottom() }
 
     stage
   }
