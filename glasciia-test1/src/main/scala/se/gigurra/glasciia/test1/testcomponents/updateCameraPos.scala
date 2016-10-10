e33f181c15f2f115b69357e4b479489d96cdc123
Johan Kjölhede <gigurra@gmail.com>
2016-10-10 22:53:34 +0200

    speedy camera

--- glasciia-test1/src/main/scala/se/gigurra/glasciia/test1/testcomponents/updateCameraPos.scala
+++ glasciia-test1/src/main/scala/se/gigurra/glasciia/test1/testcomponents/updateCameraPos.scala
@@ -1,6 +1,7 @@
 package se.gigurra.glasciia.test1.testcomponents
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input.Keys
 import se.gigurra.glasciia.{Canvas, Keyboard, Pov4W}
 import se.gigurra.glasciia.Glasciia._
 import se.gigurra.math.Vec2
@@ -10,9 +11,9 @@ import se.gigurra.math.Vec2
   */
 object updateCameraPos {
   def apply(canvas: Canvas): Unit = {
-    val worldInputKeyboard = canvas.app.resource[Keyboard]("world-input-keyboard")
-    val speed = 100.0f
-    val dr = Pov4W(worldInputKeyboard).dir.toFloat * Gdx.graphics.getDeltaTime * speed
+    val keyboard = canvas.app.resource[Keyboard]("world-input-keyboard")
+    val speed = if (keyboard.isKeyDown(Keys.SHIFT_LEFT) || keyboard.isKeyDown(Keys.SHIFT_RIGHT)) 200.0f else 100.0f
+    val dr = Pov4W(keyboard).dir.toFloat * Gdx.graphics.getDeltaTime * speed
     canvas.camera.position.set((canvas.camera.position : Vec2[Float]) + dr)
   }
 }
