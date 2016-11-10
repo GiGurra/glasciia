ce5c526b439f7f8dd9324d97c56e6c935fc60c8e
Johan Kjölhede <gigurra@gmail.com>
2016-11-11 00:09:00 +0100

    Add helper mouse pos functions

--- glasciia-core/src/main/scala/com/github/gigurra/glasciia/impl/MouseFunctions.scala
+++ glasciia-core/src/main/scala/com/github/gigurra/glasciia/impl/MouseFunctions.scala
@@ -9,5 +9,7 @@ import com.github.gigurra.math.Vec2
   */
 trait MouseFunctions {
   def mousePos: Vec2[Int] = Vec2(Gdx.input.getX, Gdx.input.getY)
+  def mousePos(pointer: Int): Vec2[Int] = Vec2(Gdx.input.getX(pointer), Gdx.input.getY(pointer))
+  def pinchPos: Vec2[Int] = (mousePos(0) + mousePos(1)) / 2
   def setCursor(cursor: Cursor): Unit = Gdx.graphics.setCursor(cursor)
 }
