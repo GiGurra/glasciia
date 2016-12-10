3b5e88eff78693779f2b0777ac7cff3a9cc2a8dd
Johan Kjölhede <gigurra@gmail.com>
2016-12-10 11:44:23 +0100

    Better Transform

--- glasciia-core/src/main/scala/com/github/gigurra/glasciia/Transform.scala
+++ glasciia-core/src/main/scala/com/github/gigurra/glasciia/Transform.scala
@@ -3,23 +3,43 @@ package com.github.gigurra.glasciia
 import java.util
 
 import com.badlogic.gdx.math.{Matrix4, Quaternion, Vector3}
-import com.github.gigurra.glasciia.impl.MatrixWithFunctions.MatrixWithFunctions
 
 import scala.language.implicitConversions
-import com.github.gigurra.math.{Vec2, Vec3, Zero}
+import com.github.gigurra.math.{Vec2, Vec3, Vec4, Zero}
 
 /**
   * Immutable transform class
   */
-case class Transform(private val m: Matrix4) { // Private because it is sucky libgdx mutable matrix
+case class Transform(private val impl: Matrix4) {
+
+  final def data: Array[Float] = impl.`val`
+
+  def *(v: Vec2[Float]): Vec2[Float] = {
+    val gdxVec = new Vector3(v.x, v.y, 0.0f)
+    gdxVec.mul4x3(data)
+    Vec2(gdxVec.x, gdxVec.y)
+  }
+
+  def *(v: Vec3[Float]): Vec3[Float] = {
+    val gdxVec = new Vector3(v.x, v.y, v.z)
+    gdxVec.mul4x3(data)
+    Vec3(gdxVec.x, gdxVec.y, gdxVec.z)
+  }
+
+  def *(v: Vec4[Float]): Vec4[Float] = {
+    val wNormalized = v.normalizeByW
+    val gdxVec = new Vector3(wNormalized.x, wNormalized.y, wNormalized.z)
+    gdxVec.mul4x3(data)
+    Vec4(gdxVec.x, gdxVec.y, gdxVec.z, 1.0f)
+  }
 
   override def hashCode(): Int = {
-    util.Arrays.hashCode(m.`val`)
+    util.Arrays.hashCode(data)
   }
 
   override def equals(obj: scala.Any): Boolean = {
     obj match {
-      case otherTransform: Transform => util.Arrays.equals(otherTransform.m.`val`, m.`val`)
+      case otherTransform: Transform => util.Arrays.equals(otherTransform.data, data)
       case _ => false
     }
   }
@@ -32,10 +52,7 @@ object Transform {
   private val Vec2One = Vec2[Float](1.0f, 1.0f)
   private val gdxZAxis = new Vector3(0.0f, 0.0f, 1.0f)
 
-  implicit def getFunctions(t: Transform): MatrixWithFunctions = new MatrixWithFunctions(t.m)
-
-  implicit def toMatrix(t: Transform): Matrix4 = t.m
-  implicit def toTransformBuilder(t: Transform): TransformBuilder = new TransformBuilder(new Matrix4(t.m))
+  implicit def toTransformBuilder(t: Transform): TransformBuilder = new TransformBuilder(new Matrix4(t.data))
   implicit def toTransform(t: TransformBuilder): Transform = new Transform(t.m)
 
   def apply(): Transform = {
@@ -61,7 +78,9 @@ object Transform {
     def scale(x: Float = 1.0f, y: Float = 1.0f, z: Float = 1.0f): TransformBuilder = mutate(_.scale(x, y, z))
     def scale(v: Vec2[Float]): TransformBuilder = scale(v.x, v.y)
     def scale(v: Vec3[Float]): TransformBuilder = scale(v.x, v.y, v.z)
+    def mul(m2: Array[Float]): TransformBuilder = mutate(m1 => Matrix4.mul(m1.`val`, m2))
     def mul(m2: Matrix4): TransformBuilder = mutate(_.mul(m2))
+    def mul(m2: Transform): TransformBuilder = mul(m2.data)
 
     private def mutate(f: Matrix4 => Unit): TransformBuilder = {
       f(m)
--- glasciia-core/src/main/scala/com/github/gigurra/glasciia/impl/ContentDrawer.scala
+++ glasciia-core/src/main/scala/com/github/gigurra/glasciia/impl/ContentDrawer.scala
@@ -2,17 +2,19 @@ package com.github.gigurra.glasciia.impl
 
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
 import com.github.gigurra.glasciia.Transform
-import Transform._
+import com.badlogic.gdx.math.Matrix4
 
 /**
   * Created by johan on 2016-10-01.
   */
 trait ContentDrawer {
 
+  private val tmp = new Matrix4
+
   def batch: PolygonSpriteBatch
 
   def draw(transform: Transform)(content: => Unit): Unit = {
-    batch.setTransformMatrix(transform)
+    batch.setTransformMatrix(tmp.set(transform.data))
     content
   }
 
