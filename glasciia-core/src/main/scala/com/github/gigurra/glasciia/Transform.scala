a6e5fae58739bdcd0e72de9af022b43f3e8d6f63
Johan Kjölhede <gigurra@gmail.com>
2016-12-30 21:52:53 +0100

    small optimizations

--- glasciia-core/src/main/scala/com/github/gigurra/glasciia/Transform.scala
+++ glasciia-core/src/main/scala/com/github/gigurra/glasciia/Transform.scala
@@ -19,20 +19,20 @@ case class Transform(private val impl: Mat4Mutable) {
   final def scaleZ: Float = impl.getScaleZ
   final def zTranslation: Float = impl.values(14)
 
-  def *(v: Vec2): Vec2 = {
+  final def *(v: Vec2): Vec2 = {
     Vec2(
       v.x * data(Matrix4.M00) + v.y * data(Matrix4.M01) + data(Matrix4.M03),
       v.x * data(Matrix4.M10) + v.y * data(Matrix4.M11) + data(Matrix4.M13))
   }
 
-  def *(v: Vec3): Vec3 = {
+  final def *(v: Vec3): Vec3 = {
     Vec3(
       v.x * data(Matrix4.M00) + v.y * data(Matrix4.M01) + v.z * data(Matrix4.M02) + data(Matrix4.M03),
       v.x * data(Matrix4.M10) + v.y * data(Matrix4.M11) + v.z * data(Matrix4.M12) + data(Matrix4.M13),
       v.x * data(Matrix4.M20) + v.y * data(Matrix4.M21) + v.z * data(Matrix4.M22) + data(Matrix4.M23))
   }
 
-  def *(v: Vec4): Vec4 = {
+  final def *(v: Vec4): Vec4 = {
     val vv: Vec4 = v.normalizeByW
     Vec4(
       vv.x * data(Matrix4.M00) + vv.y * data(Matrix4.M01) + vv.z * data(Matrix4.M02) + data(Matrix4.M03),
@@ -80,24 +80,79 @@ object Transform {
     * Used to minimize allocation of new matrices
     * Note: You should NEVER allocate or hold a reference to a TransformBuilder
     */
-  class TransformBuilder(val m: Mat4Mutable) extends AnyVal {
-    def inverse: TransformBuilder = mutate(_.invert())
-    def rotate(degrees: Float, axis: Vec3 = Z_AXIS): TransformBuilder = mutate(_.rotateDegrees(axis.x, axis.y, axis.z, degrees))
-    def translate(x: Float = 0.0f, y: Float = 0.0f, z: Float = 0.0f): TransformBuilder = mutate(_.translate(x, y, z))
-    def translate(v: Vec2): TransformBuilder = translate(v.x, v.y)
-    def translate(v: Vec3): TransformBuilder = translate(v.x, v.y, v.z)
-    def scale(x: Float = 1.0f, y: Float = 1.0f, z: Float = 1.0f): TransformBuilder = mutate(_.scale(x, y, z))
-    def scale(v: Vec2): TransformBuilder = scale(v.x, v.y)
-    def scale(v: Vec3): TransformBuilder = scale(v.x, v.y, v.z)
-    def mul(m2: Array[Float]): TransformBuilder = mutate(m1 => Matrix4.mul(m1.values, m2))
-    def mul(m2: Mat4Mutable): TransformBuilder = mutate(_.mul(m2))
-    def mul(m2: Transform): TransformBuilder = mul(m2.data)
-    def setIdentity(): TransformBuilder = mutate(_.setIdentity())
+  final class TransformBuilder(val m: Mat4Mutable) extends AnyVal {
 
-    private def mutate(f: Mat4Mutable => Unit): TransformBuilder = {
-      f(m)
+    final def inverse: TransformBuilder = {
+      m.invert()
       this
     }
+
+    final def rotate(degrees: Float, axis: Vec3 = Z_AXIS): TransformBuilder = {
+      m.rotateDegrees(axis.x, axis.y, axis.z, degrees)
+      this
+    }
+
+    final def translate(x: Float = 0.0f, y: Float = 0.0f, z: Float = 0.0f): TransformBuilder = {
+      m.translate(x, y, z)
+      this
+    }
+
+    final def translate(v: Vec2): TransformBuilder = {
+      translate(v.x, v.y)
+    }
+
+    final def translate(v: Vec3): TransformBuilder = {
+      translate(v.x, v.y, v.z)
+    }
+
+    final def preTranslate(x: Float = 0.0f, y: Float = 0.0f, z: Float = 0.0f): TransformBuilder = {
+      m.values(12) += x
+      m.values(13) += x
+      m.values(14) += x
+      this
+    }
+
+    final def preTranslate(v: Vec2): TransformBuilder = {
+      preTranslate(v.x, v.y)
+    }
+
+    final def preTranslate(v: Vec3): TransformBuilder = {
+      preTranslate(v.x, v.y, v.z)
+    }
+
+    final def scale(x: Float = 1.0f, y: Float = 1.0f, z: Float = 1.0f): TransformBuilder = {
+      m.scale(x, y, z)
+      this
+    }
+
+    final def scale(v: Vec2): TransformBuilder = {
+      scale(v.x, v.y)
+    }
+
+    final def scale(v: Vec3): TransformBuilder = {
+      scale(v.x, v.y, v.z)
+    }
+
+    final def mul(m2: Array[Float]): TransformBuilder = {
+      Matrix4.mul(m.values, m2)
+      this
+    }
+
+    final def mul(m2: Mat4Mutable): TransformBuilder = {
+      m.mul(m2)
+      this
+    }
+
+    final def mul(m2: Transform): TransformBuilder = {
+      mul(m2.data)
+      this
+    }
+
+    final def setIdentity(): TransformBuilder = {
+      m.setIdentity()
+      this
+    }
+
   }
 
   implicit def toBuilder(co: Transform.type): TransformBuilder = {
