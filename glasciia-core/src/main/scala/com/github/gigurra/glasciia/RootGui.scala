47471dbc5704cbe349b15dd01dafced22ca7a5f4
Johan Kjölhede <gigurra@gmail.com>
2017-01-31 20:35:03 +0100

    Better signals API

--- glasciia-core/src/main/scala/com/github/gigurra/glasciia/RootGui.scala
+++ glasciia-core/src/main/scala/com/github/gigurra/glasciia/RootGui.scala
@@ -3,7 +3,7 @@ package com.github.gigurra.glasciia
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatcher
-import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.{Actor, Stage}
 import com.badlogic.gdx.scenes.scene2d.ui.{Skin, Table}
 import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.viewport.ScalingViewport
@@ -19,6 +19,8 @@ class RootGui(protected val skin: Skin = new Skin,
   protected val rootTable: Table = new Table(skin)
   rootTable.setFillParent(true)
   stage.addActor(rootTable)
+
+  protected implicit def _actionRunner: Actor = stage.getRoot
 
   override def draw(canvas: Canvas,
                     dt: Float,
