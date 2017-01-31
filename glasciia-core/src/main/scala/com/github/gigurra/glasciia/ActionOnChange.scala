3add20b226a2f318d1361120e3ed52e70fdb541d
Johan Kjölhede <gigurra@gmail.com>
2017-01-31 22:17:35 +0100

    Revert useFirst

--- glasciia-core/src/main/scala/com/github/gigurra/glasciia/ActionOnChange.scala
+++ glasciia-core/src/main/scala/com/github/gigurra/glasciia/ActionOnChange.scala
@@ -5,26 +5,22 @@ import com.badlogic.gdx.scenes.scene2d.Action
 /**
   * Created by johan on 2017-01-31.
   */
-class ActionOnChange[T](getter: => T, useFirst: Boolean, action: T => Unit) extends Action {
+class ActionOnChange[T](getter: => T, action: T => Unit) extends Action {
 
   private var oldvalue: T = _
-  private var isFirst: Boolean = true
 
   override def act(delta: Float): Boolean = {
     val newValue = getter
     if (newValue != oldvalue) {
       oldvalue = newValue
-      if (!isFirst || (isFirst && useFirst)) {
-        action(newValue)
-      }
+      action(newValue)
     }
-    isFirst = false
     false
   }
 }
 
 object ActionOnChange {
-  def apply[T](getter: => T, useFirst: Boolean = false)(action: T => Unit): ActionOnChange[T] = {
-    new ActionOnChange[T](getter, useFirst, action)
+  def apply[T](getter: => T)(action: T => Unit): ActionOnChange[T] = {
+    new ActionOnChange[T](getter, action)
   }
 }
